ed4b87c36abca0a16513c0bc4328b3361ead5092
pooooh14 <113840899+pooooh14@users.noreply.github.com>
2022-12-14 01:11:53 +0530

    14/12/2022

---
+++ If-else/TwoFive.js
@@ -0,0 +1,14 @@
+var num = readInt("give me an integer");
+if(num%2 == 0 && num%5 == 0) {
+      console.log("Both"); 
+}
+else if(num%2 == 0) {
+    console.log("two");
+}
+else if(num%5 == 0) {
+    console.log("five");
+}
+else {
+    console.log("nothing");
+}
+    
